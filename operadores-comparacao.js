d16a43be4643d19855f9cd22e1ba5c1b411fb362
Ângelo Harnisch <87043148+AngeloHarnisch@users.noreply.github.com>
2024-03-18 10:55:17 -0300

    Add files via upload

---
+++ operadores-comparacao.js
@@ -0,0 +1,21 @@
+const estaAprovado = true;
+
+if (estaAprovado === true) {
+  console.log('aprovado');
+}
+
+if ('0' === 0) {
+  console.log('passou na comparação');
+} else {
+  console.log('não passou na comparação');
+}
+
+const idadeMinima = 18;
+const idadeEstudante = 16;
+
+if (idadeEstudante >= idadeMinima) {
+  console.log('não precisa de autorização');
+} else {
+  console.log('precisa de autorização');
+}
+
